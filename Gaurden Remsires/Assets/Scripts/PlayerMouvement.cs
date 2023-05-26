4611dd6789e4f14db50a47c62833faf9a8e0f1e5
OReynoud <95909515+OReynoud@users.noreply.github.com>
2023-05-26 23:16:13 +0200

    player ability to shoot ramsay and full locomotion

---
+++ Gaurden Remsires/Assets/Scripts/PlayerMouvement.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMouvement : MonoBehaviour
+{
+    public static PlayerMouvement instance;
+    public Vector3 playerDir;
+    public float speed;
+
+    public float shootingRate;
+    private float shootingTimer;
+    public float bulletSpeed;
+
+    public PlayerBullets bulletPrefab;
+
+    public int currentHealth;
+
+    public int maxHealth;
+    public Vector2 maxBounds;
+    public Vector2 minBounds;
+
+    public PlayerInputs controls;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance != null)
+        {
+            DestroyImmediate(this);
+        }
+        instance = this;
+        controls = new PlayerInputs();
+        controls.Enable();
+        controls.Player.Enable();
+        controls.Player.Move.performed += Move;
+        controls.Player.Shoot.performed += Shoot;
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        MovePlayer();
+        ShootMissiles();
+    }
+
+    void MovePlayer()
+    {
+        if (controls.Player.Move.IsPressed())
+        {
+            transform.position += playerDir * speed;
+        }
+        if (transform.position.x > maxBounds.x)
+        {
+            transform.position = new Vector3(maxBounds.x, transform.position.y, transform.position.z);
+        }
+        if (transform.position.y > maxBounds.y)
+        {
+            transform.position = new Vector3(transform.position.x, maxBounds.y, transform.position.z);
+        }
+        if (transform.position.x < minBounds.x)
+        {
+            transform.position = new Vector3(minBounds.x, transform.position.y, transform.position.z);
+        }
+        if (transform.position.y < minBounds.y)
+        {
+            transform.position = new Vector3(transform.position.x, minBounds.y, transform.position.z);
+        }
+    }
+
+    void ShootMissiles()
+    {
+        if (shootingTimer >= 0)
+        {
+            shootingTimer -= Time.fixedDeltaTime;
+        }
+        if (controls.Player.Shoot.IsPressed() && shootingTimer <= 0)
+        {
+            shootingTimer = shootingRate;
+            Instantiate(bulletPrefab.gameObject, transform.position + Vector3.right * 0.5f, Quaternion.identity);
+        }
+    }
+
+    void Move(InputAction.CallbackContext context)
+    {
+        playerDir = context.ReadValue<Vector2>().normalized;
+    }
+
+    void Shoot(InputAction.CallbackContext context)
+    {
+    }
+}
